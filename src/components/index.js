554598be4d8d52e821ef6e735b45a9d3cdefdb0f
USER <user@USERs-iMac.local>
2025-07-02 16:14:41 -0700

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Article} from './articles/Article';
-export {default as Brand} from './brand/Brand';
-export {default as Cta} from './cta/Cta';
-export {default as Feature} from './feature/Feature';
-export {default as Navbar} from './navbar/Navbar';
-export {default as Loading} from './Loading';
-export {default as ScrollToTop} from './ScrollToTop';
